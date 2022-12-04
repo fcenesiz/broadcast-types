45a043dff98047d4f446c0d6b6a0238040c181de
fcenesiz <fcenesiz@gmail.com>
2022-12-04 15:52:02 +0300

    receivers registered as dynamic

---
+++ ordered-receiver/src/main/java/com/example/broadcast_types/CustomIntentFilter.java
@@ -0,0 +1,34 @@
+package com.example.broadcast_types;
+
+import android.content.IntentFilter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomIntentFilter extends IntentFilter {
+
+    public CustomIntentFilter(Builder builder){
+            setPriority(builder.priority);
+            builder.actions.forEach(this::addAction);
+    }
+
+    public static class Builder {
+
+        private List<String> actions = new ArrayList<>();
+        private int priority;
+
+        public Builder setPriority(int priority) {
+            this.priority = priority;
+            return this;
+        }
+
+        public Builder addAction(String action){
+            actions.add(action);
+            return this;
+        }
+
+        public CustomIntentFilter build(){
+            return new CustomIntentFilter(this);
+        }
+    }
+}
--- ordered-receiver/src/main/java/com/example/broadcast_types/MainActivity.java
+++ ordered-receiver/src/main/java/com/example/broadcast_types/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.broadcast_types;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ShareCompat;
 import com.example.broadcast_types.databinding.ActivityMainBinding;
 
 /**
+ * receivers need to registered as dynamic,
  * receiver action names must be same,
  * need to use sendOrderedBroadcast() instead of sendBroadcast()
  */
 public class MainActivity extends AppCompatActivity {
+
+    private final String action = "my.same.named.action.name";
+
+    private ReceiverA receiverA = new ReceiverA();
+    private ReceiverB receiverB = new ReceiverB();
+    private ReceiverC receiverC = new ReceiverC();
 
     ActivityMainBinding binding;
 
@@ -20,10 +29,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
+
         binding.buttonRun.setOnClickListener(view ->{
-            sendOrderedBroadcast(new Intent(this, ReceiverB.class), null);
-            sendOrderedBroadcast(new Intent(this, ReceiverC.class), null);
-            sendOrderedBroadcast(new Intent(this, ReceiverA.class), null);
+            Intent intent = new Intent(action);
+            sendOrderedBroadcast(intent, null);
         });
+    }
+
+    private void registerReceivers(){
+        registerReceiver(receiverA, new CustomIntentFilter.Builder()
+                .addAction(action)
+                .setPriority(10)
+                .build());
+        registerReceiver(receiverB, new CustomIntentFilter.Builder()
+                .addAction(action)
+                .setPriority(30)
+                .build());
+        registerReceiver(receiverC, new CustomIntentFilter.Builder()
+                .addAction(action)
+                .setPriority(20)
+                .build());
+    }
+
+    private void unRegisterReceivers(){
+        unregisterReceiver(receiverA);
+        unregisterReceiver(receiverB);
+        unregisterReceiver(receiverC);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceivers();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unRegisterReceivers();
     }
 }
